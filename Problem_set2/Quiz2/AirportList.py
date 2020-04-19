4643fc290a16791af0d257fa25a0587b4f62aecb
xiaoni <xiaoni0601@gmail.com>
2020-04-19 16:08:55 +0800

    add Problem_set2

---
+++ Problem_set2/Quiz2/AirportList.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Complete the 'extract_airports()' function so that it returns a list of airport
+codes, excluding any combinations like "All".
+
+Refer to the 'options.html' file in the tab above for a stripped down version
+of what is actually on the website. The test() assertions are based on the
+given file.
+"""
+
+from bs4 import BeautifulSoup
+html_page = "/Users/xiaonili/Problem_set2/Quiz2/options.html"
+
+
+def extract_airports(page):
+    data = []
+    with open(page, "r") as html:
+        # do something here to find the necessary values
+        soup = BeautifulSoup(html, "lxml")
+        options = soup.find_all('option')
+        for o in options:
+            if len(o['value']) == 3 and o['value'] != "All":
+                data.append(o['value'])
+
+    return data
+
+
+def test():
+    data = extract_airports(html_page)
+    assert len(data) == 15
+    assert "ATL" in data
+    assert "ABR" in data
+
+if __name__ == "__main__":
+    test()
